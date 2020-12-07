949dae0b9395f78d3b9e19323baf365ab6838579
kookuo <nnaangee@gmail.com>
2020-12-08 00:40:00 +0900

    index

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -5,17 +5,10 @@ export default {
     initialColorMode: `dark`,
     colors: {
       ...novelaTheme.colors,
-      primary: '#000',
-      secondary: '#73737D',
-      accent: '#6166DC',
-      grey: '#73737D',
+      primary: '#ffffff',
+      secondary: '#ffffff',
+      accent: '#ffffff',
+      grey: '#ffffff',
       background: '#fff',
     },
-  };
-
-  const fonts = {
-    serif: "'GmarketSansMedium'",
-    sansSerif:
-      "'SF Pro Display', '-apple-system', 'BlinkMacSystemFont', 'San Francisco', 'Helvetica Neue', 'Helvetica', 'Ubuntu', 'Roboto', 'Noto', 'Segoe UI', 'Arial', sans-serif",
-    monospace: `"Operator Mono", Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`,
   };
